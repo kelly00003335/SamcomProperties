327d7956ab3ec65b7fd56f51a365b018bb465098
kelly00003335 <kelvinndungu3189@gmail.com>
2025-05-02 14:04:31 +0000

    Restored to '56fe610a397460d517cfc17b535a86c7a5a7bf13'
    Replit-Restored-To: 56fe610a397460d517cfc17b535a86c7a5a7bf13

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -75,11 +75,10 @@ const Navbar = () => {
         <div className="container mx-auto px-4">
           <div className="flex justify-between items-center">
             <Link href="/" className="flex items-center">
-              <img 
-                src="https://i.imgur.com/81eAvY3.jpeg" 
-                alt="Samcom Properties Logo" 
-                className="h-12 w-auto"
-              />
+              <div className="text-2xl font-bold font-heading">
+                <span className="text-[#1A237E]">SAMCOM</span>
+                <span className="text-[#FFD700] ml-1">PROPERTIES</span>
+              </div>
             </Link>
             
             {/* Desktop Menu */}
